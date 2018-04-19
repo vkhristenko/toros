b5689fc8eefda9204211e8eaab5ee620df8b6bfc
vkhristenko <vdkhristenko1991@gmail.com>
2018-04-19 11:13:06 +0200

    except for 2 bytes, reading fine

--- apps/shared/src/main/scala/ch/cern/toros/apps/test_io.scala
+++ apps/shared/src/main/scala/ch/cern/toros/apps/test_io.scala
@@ -82,6 +82,72 @@ object test_io {
     root_key(buffer)
   }
 
+  def read_tkey(buffer: ByteBuffer): Unit = {
+    println("="*50)
+    println("ROOT TKey: ")
+    println("="*50)
+    println(s"buffer current position = ${buffer.position}")
+
+    //
+    val lObjCompressed = buffer.getInt
+    println(s"lObjCompressed = $lObjCompressed")
+
+    // 
+    val keyVersion = buffer.getShort
+    println(s"keyVersion = $keyVersion")
+
+    //
+    val lObjUncompressed = buffer.getInt
+    println(s"lObjUncompressed = $lObjUncompressed")
+    
+    // 
+    val datime = buffer.getInt
+    println(s"datime = $datime")
+
+    // 
+    val keyLen = buffer.getShort
+    println(s"keyLen = $keyLen")
+
+    //
+    val cycle = buffer.getShort
+    println(s"cycle = $cycle")
+
+    //
+    val p2Key = buffer.getInt
+    println(s"p2Key = $p2Key")
+
+    //
+    val p2Dir = buffer.getInt
+    println(s"p2Dir = $p2Dir")
+
+    //
+    val nBytesClassName = buffer.get
+    println(s"nBytesClassName = $nBytesClassName")
+
+    print("class name = ")
+    for (i <- 0 until nBytesClassName)
+      print(buffer.get.toChar)
+    println("")
+
+    //
+    val lname = buffer.get
+    println(s"lname = $lname")
+    print("object name = ")
+    for (i <- 0 until lname)
+      print(buffer.get.toChar)
+    println("")
+
+    //
+    val ltitle = buffer.get
+    println(s"ltitle = $ltitle")
+    print("title name = ")
+    for (i <- 0 until ltitle)
+      print(buffer.get.toChar)
+    println("")
+
+    println(s"buffer position = ${buffer.position}")
+  }
+ 
   def root_key(buffer: ByteBuffer): Unit = {
     println("="*50)
     println("ROOT TKey: ")
@@ -147,6 +213,13 @@ object test_io {
 
     println(s"buffer position = ${buffer.position}")
 
+    root_directory(buffer)
+  }
+  
+  def root_directory(buffer: ByteBuffer): Unit = {
+    println("="*50)
+    println("ROOT TDirectory:")
+    println("="*50)
     val len = buffer.get
     println(s"len = $len")
     for (i <- 0 until len)
@@ -160,10 +233,7 @@ object test_io {
     println(s"len1 = $len1")
     println(s"buffer position = ${buffer.position}")
 
-    root_directory(buffer)
-  }
-  
-  def root_directory(buffer: ByteBuffer): Unit = {
+
     val v = buffer.getShort
     println(s"version = $v")
 
@@ -200,10 +270,91 @@ object test_io {
     //
     buffer.position(fseekkeys)
     println(s"current position = ${buffer.position}")
+
+    read_tkey(buffer)
     
-    //
-    val nbytes = buffer.getInt
-    println(s"nbytes = $nbytes")
+    val nkeys = buffer.getInt
+    println(s"nkeys = $nkeys")
+  }
+
+  def read_free(buffer: ByteBuffer): Unit = {
+    val version = buffer.getShort
+    println(s"version = $version")
+
+    val first = buffer.getInt
+    println(s"first = $first")
+
+    val last = buffer.getInt
+    println(s"last = $last")
+  }
+
+  def read_tobject(buffer: ByteBuffer): Unit = {
+    println("="*50)
+    println("TObject: ")
+    println("="*50)
+
+    val id = buffer.getInt
+    println(s"id = $id")
+
+    val bits = buffer.getInt
+    println(s"bits = $bits")
+  }
+
+  def read_tstring(buffer: ByteBuffer): Unit = {
+    println("="*50)
+    println("TString: ")
+    println("="*50)
+
+    val sss = buffer.get
+    println(s"string size = $sss")
+
+    val str = (for (i <- 0 until sss) yield buffer.get.toChar).mkString
+    println(s"string = $str")
+  }
+
+  def read_tlist(buffer: ByteBuffer): Unit = {
+    println("="*50)
+    println("TList: ")
+    println("="*50)
+
+    println(s"current position = ${buffer.position}")
+    println(s"capacity = ${buffer.capacity}")
+
+    val version = buffer.getShort
+    val version_str = "%02X" format version
+    println(s"version = ${version_str}")
+    val spart = buffer.getShort
+
+    val bytecount = ((version & 0x3fff) << 16) + spart
+    println(s"bytecount = $bytecount")
+
+    val vv = buffer.getShort
+    println(s"actual version = $vv")
+
+    read_tobject(buffer)
+    read_tstring(buffer)
+
+    val fsize = buffer.getInt
+    println(s"fsize = $fsize")
+
+    val b1 = buffer.get
+    println(s"b1 = $b1")
+
+    val b2 = buffer.get
+    println(s"b2 = $b2")
+  }
+
+  def read_streamer(buffer: ByteBuffer): Unit = {
+    println("="*50)
+    println("Streamer Info:")
+    println("="*50)
+    
+    println(s"current position = ${buffer.position}")
+    println(s"capacity = ${buffer.capacity}")
+
+//    val version = 
+    read_tkey(buffer)
+    read_tlist(buffer)
   }
 
   def test_root_header(path: String): Unit = {
@@ -215,7 +366,7 @@ object test_io {
     println(s"channel position = ${ch.position}") 
 
     // get a byte buffer and read the first 
-    val bbuffer = ByteBuffer.allocateDirect(300);
+    val bbuffer = ByteBuffer.allocateDirect(ch.size.toInt);
     ch.read(bbuffer)
     // have to rewind the buffer back to 0
     bbuffer.rewind
@@ -226,7 +377,33 @@ object test_io {
     println(s"bbuffer position = ${bbuffer.position}")
 
     root_header(bbuffer)
+    dump_raw_buffer(bbuffer)
+
+    bbuffer.position(346)
+    read_tkey(bbuffer)
+    read_free(bbuffer);
+    dump_raw_buffer(bbuffer)
+
+    dump_raw_buffer(bbuffer)
+    bbuffer.position(261)
+    read_streamer(bbuffer)
+    dump_raw_buffer(bbuffer)
+
   }
+  
+  def dump_raw_buffer(buffer: ByteBuffer, ngroups: Int = 25): Unit =
+    if (buffer.hasArray) {
+      println(s"current position = ${buffer.position}")
+      println(s"capacity = ${buffer.capacity}")
+      dump_raw(buffer.array());
+    }
+    else {
+      val arr = Array.fill[Byte](buffer.capacity - buffer.position)(0)
+      println(s"current position = ${buffer.position}")
+      println(s"capacity = ${buffer.capacity}")
+      buffer.get(arr)
+      dump_raw(arr);
+    }
 
   def dump_raw(bytes: Array[Byte], ngroups: Int = 25): Unit = {
     for (i <- 0 until 25) if (i<10) print(s"$i  ") else print(s"$i ")
